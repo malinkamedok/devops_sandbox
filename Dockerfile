6f578edc9ad52a3e1227580552261edf52816b78
pavel <pavel_solovev21@mail.ru>
2024-03-28 03:21:35 +0300

    feat: add Dockerfile and tests

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,32 @@
-FROM ubuntu:22.04@sha256:77906da86b60585ce12215807090eb327e7386c8fafb5402369e421f44eff17e
+FROM ubuntu:23.10@sha256:50ec5c3a1814f5ef82a564fae94f6b4c5d550bb71614ba6cfe8fadbd8ada9f12
+ARG GO_VERSION=go1.22.1
+ARG PYTHON_VERSION=3.12
+RUN apt update -y -q && apt upgrade -y -q
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN apt install --no-install-recommends -y -q \
+    curl \
+    build-essential \
+    ca-certificates \
+    git \
+    make \
+    gcc \
+    g++ \
+    gnupg \
+    unixodbc-dev
+
+# Install Go
+
+RUN curl -s https://dl.google.com/go/${GO_VERSION}.linux-amd64.tar.gz | tar xz -C /usr/local
+ENV PATH $PATH:/usr/local/go/bin
+
+
+## Install Python
+RUN apt install --no-install-recommends -y -q \
+  python${PYTHON_VERSION}-full \
+  python3-pip
+
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 2
--- tests/test_default_packages.py
+++ tests/test_default_packages.py
@@ -21,67 +21,21 @@ def host(request):
     yield testinfra.get_host("docker://" + docker_id)
     subprocess.check_call(["docker", "rm", "-f", docker_id])
 
-
-def test_python_version(host: Host):
-    version = StrictVersion(
-        host.run("python --version | awk '{print $2}'").stdout.strip()
-    )
-    desired_version = StrictVersion("3.11.7")
-    assert version == desired_version
-
-
 @pytest.mark.parametrize(
-    ["package", "version"],
+    ["package", "command", "version"],
     [
-        ("nghttp2", "1.57.0"),
-        ("libssl3", "3.1.4"),
-        ("libcurl", "8.5.0"),
-        ("libcrypto3", "3.1.4"),
+        ("go", "go version", "1.22.1"),
+        ("python", "python --version", "3.12"),
     ],
+    ids=["go-1.22.1", "python-3.12"]
 )
-def test_packages_versions(host: Host, package: str, version: str):
-    actual_version = StrictVersion(
-        host.run(
-            f"apk list | grep ^{package}"
-            " | sort | head -n 1 | awk '{print $1}' | cut -d'-' -f2"
-        ).stdout.strip()
-    )
-    desired_version = StrictVersion(version)
-    assert desired_version >= actual_version
-
+def test_packages_versions(host: Host, package: str, command: str, version: str):
+    output = host.run(command).stdout.strip()
+    assert version in output
 
 @pytest.mark.parametrize(
     ["package", "version"],
-    [
-        ("make", "4.4.1"),
-        ("gcc", "12.2.1"),
-        ("git", "2.40.1"),
-        ("tar", "1.34"),
-        ("xz", "5.4.3"),
-    ],
-)
-def test_essential_utilities_versions(host: Host, package: str, version: str):
-    if package in ["tar", "xz"]:
-        actual_version = StrictVersion(
-            host.run(
-                f"{package} --version"
-                " | head -n 1 | awk '{print $4}' | cut -d '_' -f 1"
-            ).stdout.strip()
-        )
-    else:
-        actual_version = StrictVersion(
-            host.run(
-                f"{package} --version"
-                " | head -n 1 | awk '{print $3}' | cut -d '_' -f 1"
-            ).stdout.strip()
-        )
-    desired_version = StrictVersion(version)
-    assert desired_version >= actual_version
-
-
-@pytest.mark.parametrize(
-    ["package", "version"],
-    [("pip", "23.3.1"), ("virtualenv", "20.25.0")],
+    [("pip", "23.2")],
 )
 def test_python_modules_version(host: Host, package: str, version: str):
     actual_version = StrictVersion(
